288c8098c52e3bac2257c2bec4fd274be07a97f5
evp2 <evan@Evans-MacBook-Air.local>
2025-11-13 11:48:08 -0500

    Update Client

--- oauthclient/src/main/java/com/github/evp2/oauthclient/controller/ClientController.java
+++ oauthclient/src/main/java/com/github/evp2/oauthclient/controller/ClientController.java
@@ -5,27 +5,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
+import reactor.core.publisher.Mono;
 
 @RestController
 @ResponseBody
 public class ClientController {
   @GetMapping("/")
-  public Map<String, String> index(Authentication authentication) {
-    return Map.of("Welcome", authentication.getName());
+  public Mono<String> index(Authentication authentication) {
+    return Mono.just("Welcome " + authentication.getName());
   }
 
   @RequestMapping("/fallback")
-  public String fallback() {
-    return "There was a downstream error, this is a fallback.";
+  public Mono<String> fallback() {
+    return Mono.just("There was a downstream error, this is a fallback.");
   }
 
   @GetMapping("/logout")
-  public String logout(Authentication authentication) {
-    if (authentication != null) {
-      authentication.setAuthenticated(false);
-    }
-    return "Logged out successfully.";
+  public Mono<String> logout() {
+    return Mono.just("Logged out successfully.");
   }
 }
