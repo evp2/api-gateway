297893fce386209c1c22986f534047ec8904e1fa
evp2 <evan@Evans-MacBook-Air.local>
2025-11-25 09:53:01 -0500

    Cleaning up refactoring

---
+++ oauthclient/src/main/java/com/github/evp2/gatewaymvc/LoginGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.github.evp2.gatewaymvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginGatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LoginGatewayApplication.class, args);
+  }
+
+}
